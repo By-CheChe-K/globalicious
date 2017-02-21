290fdd50e29d9ce9c3a709f1e789af64fc76cb26
By-CheChe-K <baichechekeii@gmail.com>
2017-02-21 22:22:34 +0900

    Add Comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220074043) do
+ActiveRecord::Schema.define(version: 20170221091634) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.text     "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
